8d584152e749cc20ddbc8b357e8fdb135ad8eceb
Robert <rlhead94@gmail.com>
2023-02-23 13:09:34 -0500

    arrow function hw

---
+++ app.js
@@ -0,0 +1,45 @@
+function addTwo(x,y){
+    console.log(x + y)
+    return x + y
+}
+let varOne = 3
+let varTwo = 4
+addTwo(varOne,varTwo)
+function subTwo(x,y){
+    console.log(x - y)
+    return x - y 
+}
+subTwo(varOne,varTwo)
+function multTwo(x,y){
+    console.log(x * y)
+    return x * y
+}
+multTwo(varOne,varTwo)
+function divTwo(x,y){
+    console.log(x / y)
+    return x / y
+}
+divTwo(varOne,varTwo)
+
+const sum = (x,y) => {
+    x + y
+    console.log(x + y)
+}
+
+const sub = (x,y) => {
+    x - y
+    console.log(x - y)
+}
+const multp = (x,y) =>{
+    x * y
+    console.log(x * y)
+}
+const divide = (x / y) => {
+    x / y
+    console.log(x / y)
+
+}
+sum(varOne,varTwo)
+sub(varOne,varTwo)
+multp(varOne,varTwo)
+divide (varOne,varTwo)
